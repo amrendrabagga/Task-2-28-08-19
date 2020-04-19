eb6a0bf48ad601954f2999ecdde705430ab0f087
Amrendra Bagga <amrendrabagga@gmail.com>
2020-04-19 16:39:25 +0530

    fixing indentation in HDFCCard and CARD

--- src/transaction/CARD.java
+++ src/transaction/CARD.java
@@ -1,10 +1,8 @@
 package transaction;
 
 public interface CARD {
-	
 	void deposit();
 	void withdraw();
 	void cardExpiry();
 	void changePIN();
-	
 }
--- src/transaction/HDFCCard.java
+++ src/transaction/HDFCCard.java
@@ -1,8 +1,8 @@
 package transaction;
 
-public class HDFCCard implements CARD{
+public class HDFCCard implements CARD {
 
-	String cardExpiry="25-aug-24";
+	String cardExpiry="25-OCT-24";
 	
 	@Override
 	public void changePIN() {
@@ -12,20 +12,15 @@ public class HDFCCard implements CARD{
 	@Override
 	public void withdraw() {
 		System.out.println("CASH WITHDRAWN FROM HDFC CARD SUCCESSFULLY");
-		
 	}
 
 	@Override
 	public void cardExpiry() {
-		System.out.println("HDFC CARD EXPIRY IS "+cardExpiry);
-		
+		System.out.println("HDFC CARD EXPIRY IS " + cardExpiry);
 	}
 
 	@Override
 	public void deposit() {
 		System.out.println("CASH DEPOSITED SUCCESSFULLY INTO HDFC CARD");
-		
 	}
-
-	
 }
